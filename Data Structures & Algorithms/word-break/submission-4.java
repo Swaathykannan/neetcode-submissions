a694fd928633893f82fc44b1025d8184d26268ec
Swaathykannan <34771659+Swaathykannan@users.noreply.github.com>
2026-08-16 15:23:16 +0530

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashMap<Integer, Boolean> cache = new HashMap<>();
+        cache.put(s.length(), true);
+        return isWordBreak(s, 0, wordDict, cache);
+    }
+
+    private boolean isWordBreak(
+        String s, int i, List<String> wordDict, HashMap<Integer, Boolean> cache) {
+        if (cache.containsKey(i)) {
+            return cache.get(i);
+        }
+        for (String word : wordDict) {
+            if (i + word.length() <= s.length() && s.substring(i, i + word.length()).equals(word)) {
+                if (isWordBreak(s, i + word.length(), wordDict, cache)) {
+                    cache.put(i, true);
+                    return true;
+                };
+            }
+        }
+        cache.put(i, false);
+        return false;
+    }
+}
